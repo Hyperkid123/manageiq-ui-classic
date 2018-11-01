0e65d2c8f781ad09f020b81ed844ac0f11cf10a6
Martin Maroši <marvusm.mmi@gmail.com>
2018-11-01 15:31:15 +0100

    Fixed no-unused-vars eslint issues.

--- app/assets/javascripts/miq_application.js
+++ app/assets/javascripts/miq_application.js
@@ -496,7 +496,7 @@ function miqBuildChartMenu(col, row, _value, category, series, id, _message) {
   }
 }
 
-function miqChartBindEvents(chart_set, chart_index) {
+function miqChartBindEvents() {
   if (ManageIQ.charts.provider === 'c3') {
     // noop
   }
@@ -1201,7 +1201,7 @@ function miqInitCodemirror(options) {
     viewportMargin: Infinity,
   });
 
-  ManageIQ.editor.on('change', function(cm, change) {
+  ManageIQ.editor.on('change', function() {
     if (options.angular) {
       ManageIQ.editor.save();
       $(textarea).trigger('change');
@@ -1210,7 +1210,7 @@ function miqInitCodemirror(options) {
     }
   });
 
-  ManageIQ.editor.on('blur', function(cm, change) {
+  ManageIQ.editor.on('blur', function() {
     ManageIQ.editor.save();
   });
 
